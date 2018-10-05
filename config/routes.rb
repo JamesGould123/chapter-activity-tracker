81e0b885ca165e0cd0926be06365cb13b7b5c206
Tyler Williams <tyler@ogdenstudios.xyz>
2018-10-05 12:20:45 -0600

    add chapters controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'chapters/index'
+
+  devise_for :users, controllers: {
+   sessions: 'users/sessions'
+ }
+
 
   root to: 'static_pages#index'
   get '/faq',                  to: 'static_pages#faq'
   get '/store',                to: 'static_pages#store'
+  get '/chapters/',            to: 'chapters#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
