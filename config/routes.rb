d7b2586d8961fe0bd21fc737c43d4af3e22f1a4e
Tyler Williams <tyler@ogdenstudios.xyz>
2018-10-05 11:32:20 -0600

    add faq

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: 'static_pages#index'
-  get 'static_pages/index'
+  get '/faq', to: 'static_pages#faq'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
