5a19306bb713bac6b18dba7bd1b8452061dbca67
Tyler Williams <tyler@ogdenstudios.xyz>
2018-10-17 15:27:58 -0400

    add slug attribute to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181016231943) do
+ActiveRecord::Schema.define(version: 20181017192732) do
 
   create_table "buckets", force: :cascade do |t|
     t.string "name"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20181016231943) do
     t.datetime "updated_at", null: false
     t.string "permissions"
     t.string "name"
+    t.string "slug"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
