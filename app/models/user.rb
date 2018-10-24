834181cd556c1a0f8842f48ee5415ea3fb93c2f2
Tyler Williams <tyler@ogdenstudios.xyz>
2018-10-24 16:19:46 -0600

    add up to 14.2 for following users in hartl tutorial

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,20 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
   has_many :chapter_actions
   has_many :spendings
+  has_many :active_relationships, class_name:   "Relationship",
+  foreign_kkey: "follower_id",
+  dependent:    :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                 foreign_key: "followed_id",
+                                 dependent:   :destroy
 
-#TODO: this should just be one method, instead of four for each criteria
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
+  #TODO: this should just be one method, instead of four for each criteria
   def is_hq_cb?
     cb_points = 0
     cb_actions = ChapterAction.where("id = #{self.id}")
@@ -47,5 +56,20 @@ class User < ApplicationRecord
       points += action.points
     end
     return points > 50
+  end
+
+  # Follows a user.
+  def follow(other_user)
+    following << other_user
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
