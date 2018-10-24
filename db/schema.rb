22d579997afdaf1c7768a25d488aff1feea55a41
Tyler Williams <tyler@ogdenstudios.xyz>
2018-10-24 15:28:26 -0600

    create relationships model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181024180120) do
+ActiveRecord::Schema.define(version: 20181024212724) do
 
   create_table "buckets", force: :cascade do |t|
     t.string "name"
@@ -39,6 +39,16 @@ ActiveRecord::Schema.define(version: 20181024180120) do
     t.datetime "date_completed"
     t.index ["bucket_id"], name: "index_chapter_actions_on_bucket_id"
     t.index ["user_id"], name: "index_chapter_actions_on_user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "reports", force: :cascade do |t|
