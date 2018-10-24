76713767a50bac9fd0a23048f587b12d49c93481
Tyler Williams <tyler@ogdenstudios.xyz>
2018-10-24 14:06:47 -0600

    add report email functionality which sends out dummy strings on the rake task, using report's send method

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -5,4 +5,9 @@ class ReportMailer < ApplicationMailer
     puts "Sending sample email"
     mail(to: "tyler.williams@ssdp.org", subject: 'Sample Email')
   end
+
+  def report_email(report)
+    @report = report
+    mail(to: "tyler.williams@ssdp.org", subject: 'CAT Report')
+  end
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,9 +1,6 @@
 class Report < ApplicationRecord
   def send
-    puts self.chapter_universe
-    puts self.task_universe
-    puts self.time_window
     # Send an email with this information
-    ReportMailer.sample_email().deliver
+    ReportMailer.report_email(self).deliver
   end
 end
