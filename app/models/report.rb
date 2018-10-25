5d15baf522561c109be50532844dd4d886ad2709
Tyler Williams <tyler@ogdenstudios.xyz>
2018-10-25 16:12:46 -0600

    printing out all activities by followed accounts in console

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,6 +1,17 @@
 class Report < ApplicationRecord
   def send_report_email
+    #  Find all the ids of people I follow
+    user = User.first
+    following_ids = user.following_ids
+    following_ids.each do |following_id|
+      followed_account = User.find(following_id)
+      puts followed_account.name
+      actions =  ChapterAction.where("user_id = '#{followed_account.id}'")
+      actions.each do |action|
+        puts Category.find(action.category_id).name
+      end
+    end
     # Send an email with this information
-    ReportMailer.report_email(self).deliver
+    #ReportMailer.report_email(self).deliver
   end
 end
