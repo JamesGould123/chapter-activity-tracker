24203cfd6e7b567c2fd290285c82e2d2b35e0ddd
Tyler Williams <tyler@ogdenstudios.xyz>
2018-10-26 14:32:48 -0600

    add some more placeholder comments and code in report model to plan out final steps here, before branching and trying something fancy

--- app/models/report.rb
+++ app/models/report.rb
@@ -6,6 +6,8 @@ class Report < ApplicationRecord
     following_ids.each do |following_id|
       followed_account = User.find(following_id)
       actions =  ChapterAction.where("user_id = '#{followed_account.id}'")
+      # Date selection code:
+      ChapterAction.where(:date_completed => Date.new(2018, 1, 1)..Date.new(2018, 10, 1))
       action_data = []
       actions.each do |action|
         description = Category.find(action.category_id).name
@@ -16,3 +18,12 @@ class Report < ApplicationRecord
     ReportMailer.report_email(data).deliver
   end
 end
+
+# 1. scheduler rake task fires one task
+# 2. That task runs through the list of all reports
+# 3. Each report runs send_report_email with options
+# 4. So each report needs to know:
+## - Recipient address
+## - Recipient id to get following ids
+## - How often it should fire (daily or weekly?)
+## - Action ID filter 
